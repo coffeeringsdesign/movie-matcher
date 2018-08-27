ccaf77da54e0217bad530becf8b667cca63a740f
Kendra Kelly and Maly Phongsavanh <maly@>
2018-08-27 13:15:12 -0700

    add in prototype to sort through emotions and interface to show appropriate divs

--- js/scripts.js
+++ js/scripts.js
@@ -4,31 +4,44 @@ function Movie(title, category) {
   this.category = category;
 }
 
-var titanic = new Movie("Titanic", sad);
+var titanic = new Movie("Titanic", "sad");
+// var fault = new Movie("Fault In Our Stars", "sad");
 
-Movie.prototype.mood = function() {
-  if (moodchoice === "sad") {
-    sad();
-  // } else if (moodchoice === "happy") {
-  //   happy.category.fadeIn();
-  // } else if (moodChoice === "angst") {
-  //   angst.category.fadeIn();
-  // } else if (moodChoice === "love") {
-  //   love.category.fadeIn();
-  // }
-  // return;
+Movie.prototype.mood = function(moodChoice) { //beingtriggered
+  if (moodChoice === "sad") {
+    return 1;
+  } else if (moodChoice === "happy") {
+    return 2;
+  } else if (moodChoice === "angst") {
+    return 3;
+  } else if (moodChoice === "love") {
+    return 4;
+  }
 }
 
 // user interface
+// var sad = function() {
+//   (this['category'] = "sad").show();
+// }
+
 $(document).ready(function() {
+  var newMovie = new Movie("title", "category");
+
   $("#moodForm").submit(function(event) {
     event.preventDefault();
-    var moodChoice = ($("input[name='moods']:checked").val());
-    mood(moodChoice);
-  });
+    var moodChoice = ($("input[name='optionsRadios']:checked").val());
+    // console.log(moodChoice);
+      if (newMovie.mood(moodChoice) === 1) {
+        $(".sad").fadeIn();
+      } else if (newMovie.mood(moodChoice) === 2) {
+        $(".happy").fadeIn();
+      } else if (newMovie.mood(moodChoice) === 3) {
+        $(".angst").fadeIn();
+      } else if (newMovie.mood(moodChoice) === 4) {
+        $(".love").fadeIn();
+      }
 
-  sad fuction() {
-    (this['category'] = "sad").show();
-  }
+    // var newMovie = new Movie(movieTitle, moodChoice);
+  });
 
 });
