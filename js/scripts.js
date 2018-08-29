a9d3386727df137109eb0e598be7c3c49e3af0c3
Kendra <kendra@coffeeringsdesign.com>
2018-08-29 15:58:22 -0700

    simplify code and remove notes

--- js/scripts.js
+++ js/scripts.js
@@ -13,16 +13,12 @@ var lionKing = new Movie("The Lion King", 5, 3, 0, 3, "img/lion-king.jpg", "A Li
 var soundOf = new Movie("The Sound of Music", 4, 2, 1, 3, "img/sound-of-music.jpg", "A woman leaves an Austrian convent to become a governess to the children of a Naval officer widower.", "https://www.imdb.com/title/tt0059742/");
 var homeAlone = new Movie("Home Alone", 5, 0, 1, 0, "img/home-alone.jpg", "An eight-year-old troublemaker must protect his house from a pair of burglars when he is accidentally left home alone by his family during Christmas vacation.", "https://www.imdb.com/title/tt0099785/");
 var shrek = new Movie("Shrek", 5, 1, 1, 4, "img/shrek.jpg", "After his swamp is filled with magical creatures, Shrek agrees to rescue Princess Fiona for a villainous lord in order to get his land back.", "https://www.imdb.com/title/tt0126029/");
-
-// sad movies
 var titanic = new Movie("Titanic", 0, 5, 0, 3, "img/titanic.jpg", "A seventeen-year-old aristocrat falls in love with a kind but poor artist aboard the luxurious, ill-fated R.M.S. Titanic.", "https://www.imdb.com/title/tt0120338/");
 var fault = new Movie("Fault In Our Stars", 2, 4, 0, 4, "img/the-fault-in-our-stars.jpg", "Two teenage cancer patients begin a life-affirming journey to visit a reclusive author in Amsterdam.", "https://www.imdb.com/title/tt2582846/");
 var schindlers = new Movie("Schindler's List", 0, 5, 0, 0, "img/schindlers-list.jpg", "In German-occupied Poland during World War II, Oskar Schindler gradually becomes concerned for his Jewish workforce after witnessing their persecution by the Nazi Germans.", "https://www.imdb.com/title/tt0108052/");
 var alice = new Movie("Still Alice", 1, 3, 0, 1, "img/still-alice.jpg", "A linguistics professor and her family find their bonds tested when she is diagnosed with Alzheimer's Disease.", "https://www.imdb.com/title/tt3316960/");
 var coco = new Movie("Coco", 4, 2, 1, 3, "img/coco.jpg", "Aspiring musician Miguel, confronted with his family's ancestral ban on music, enters the Land of the Dead to find his great-great-grandfather, a legendary singer.", "https://www.imdb.com/title/tt2380307");
 var findingNemo = new Movie("Finding Nemo", 3, 1, 2, 3, "img/nemo.jpg", "After his son is captured in the Great Barrier Reef and taken to Sydney, a timid clownfish sets out on a journey to bring him home.", "https://www.imdb.com/title/tt0266543");
-
-// love movies
 var sleepless = new Movie("Sleepless In Seattle", 4, 0, 0, 5, "img/sleepless-in-seattle.jpg", "A recently widowed man's son calls a radio talk-show in an attempt to find his father a partner.", "https://www.imdb.com/title/tt0108160/");
 var firstDate = new Movie("50 First Dates", 4, 1, 2, 5, "img/50-first-dates.jpg", "Henry Roth is a man afraid of commitment up until he meets the beautiful Lucy. They hit it off and Henry think he's finally found the girl of his dreams, until he discovers she has short-term memory loss and forgets him the next day.", "https://www.imdb.com/title/tt0343660/");
 var tenThings = new Movie("10 Things I Hate About You", 4, 0, 1, 5, "img/10things.jpg", "A pretty, popular teenager can't go out on a date until her ill-tempered older sister does.", "https://www.imdb.com/title/tt0147800/");
@@ -33,8 +29,6 @@ var eternalSunshine = new Movie("Eternal Sunshine of the Spotless Mind", 0, 3, 3
 var loveActually = new Movie("Love Actually", 4, 2, 1, 5, "img/love-actually.jpg", "Follows the lives of eight very different couples in dealing with their love lives in various loosely interrelated tales all set during a frantic month before Christmas in London, England.", "https://www.imdb.com/title/tt0314331/");
 var clueless = new Movie("Clueless", 4, 0, 1, 3, "img/clueless.jpg", "A rich high school student tries to boost a new pupil's popularity, but reckons without affairs of the heart getting in the way.", "https://www.imdb.com/title/tt0112697/");
 var grease = new Movie("Grease", 4, 0, 1, 3, "img/grease.jpg", "Good girl Sandy and greaser Danny fell in love over the summer. When they unexpectedly discover they're now in the same high school, will they be able to rekindle their romance?", "https://www.imdb.com/title/tt0077631/");
-
-// angst movies
 var kungFu = new Movie("Kung Fu Hustle", 0, 0, 5, 0, "img/kung-fu-hustle.jpg", "In Shanghai, China in the 1940s, a wannabe gangster aspires to join the notorious 'Axe Gang' while residents of a housing complex exhibit extraordinary powers in defending their turf.", "https://www.imdb.com/title/tt0373074/");
 var taken = new Movie("Taken", 0, 1, 4, 0, "img/taken.jpg", "A retired CIA agent travels across Europe and relies on his old skills to save his estranged daughter, who has been kidnapped while on a trip to Paris.", "https://www.imdb.com/title/tt0936501/");
 var fightClub = new Movie("Fight Club", 1, 0, 3, 0, "img/fight-club.jpg", "An insomniac office worker and a devil-may-care soapmaker form an underground fight club that evolves into something much, much more.", "https://www.imdb.com/title/tt0137523/");
@@ -42,9 +36,20 @@ var tarzan = new Movie("Tarzan", 0, 0, 2, 0, "img/tarzan.jpg", "A man raised by 
 var perksWallflower = new Movie("The Perks of Being a Wallflower", 1, 2, 1, 1, "img/perks.jpg", "An introvert freshman is taken under the wings of two seniors who welcome him to the real world.", "https://www.imdb.com/title/tt1659337/");
 var prada = new Movie("The Devil Wears Prada", 3, 1, 4, 0, "img/prada.jpg", "A smart but sensible new graduate lands a job as an assistant to Miranda Priestly, the demanding editor-in-chief of a high fashion magazine.", "https://www.imdb.com/title/tt0458352/")
 var starWars = new Movie("Star Wars: Episode IV - A New Hope", 3, 3, 4, 2, "img/star-wars.jpg", "Luke Skywalker joins forces with a Jedi Knight, a cocky pilot, a Wookiee and two droids to save the galaxy from the Empire's world-destroying battle station, while also attempting to rescue Princess Leia from the evil Darth Vader.", "https://www.imdb.com/title/tt0076759/")
-
-
-// movies et al
+var meetJoeBlack = new Movie("Meet Joe Black", 1, 3, 3, 3, "img/meet-joe-black.jpg", "Death, who takes the form of a young man, asks a media mogul to act as a guide to teach him about life on Earth, and in the process, he falls in love with his guide's daughter.", "http://www.imdb.com/title/tt0119643");
+var departed = new Movie("The Departed", 0, 2, 5, 0, "img/departed.jpg", "An undercover cop and a mole in the police attempt to identify each other while infiltrating an Irish gang in South Boston.", "https://www.imdb.com/title/tt0407887");
+var hook = new Movie("Hook", 4, 1, 2, 2, "img/hook.jpg", "When Captain Hook kidnaps his children, an adult Peter Pan must return to Neverland and reclaim his youthful spirit in order to challenge his old enemy.", "https://www.imdb.com/title/tt0102057");
+var leagueofTheirOwn = new Movie("A League of Their Own", 3, 1, 2, 2, "img/league-of-their-own.jpg", "Two sisters join the first female professional baseball league and struggle to help it succeed amidst their own growing rivalry.", "https://www.imdb.com/title/tt0104694");
+var iamLegend = new Movie("I Am Legend", 0, 5, 4, 0, "img/i-am-legend.jpg", "Years after a plague kills most of humanity and transforms the rest into monsters, the sole survivor in New York City struggles valiantly to find a cure.", "https://www.imdb.com/title/tt0480249");
+var getOut = new Movie("Get Out", 0, 2, 5, 1, "img/get-out.jpg", "A young African-American visits his white girlfriend's parents for the weekend, where his simmering uneasiness about their reception of him eventually reaches a boiling point.", "https://www.imdb.com/title/tt5052448");
+var aQuietPlace = new Movie("A Quiet Place", 0, 5, 5, 1, "img/quiet-place.jpg", "In a post-apocalyptic world, a family is forced to live in silence while hiding from monsters with ultra-sensitive hearing.", "https://www.imdb.com/title/tt6644200");
+var rbg = new Movie("RBG", 3, 1, 2, 3, "img/rbg.jpg", "The exceptional life and career of U.S. Supreme Court Justice Ruth Bader Ginsburg, who has developed a breathtaking legal legacy while becoming an unexpected pop culture icon.", "https://www.imdb.com/title/tt7689964");
+var wonderWoman = new Movie("Wonder Woman", 2, 2, 3, 3, "img/wonder-woman.jpg", "When a pilot crashes and tells of conflict in the outside world, Diana, an Amazonian warrior in training, leaves home to fight a war, discovering her full powers and true destiny.", "https://www.imdb.com/title/tt0451279");
+var notebook = new Movie("The Notebook", 3, 4, 2, 5, "img/notebook.jpg", "A poor yet passionate young man falls in love with a rich young woman, giving her a sense of freedom, but they are soon separated because of their social differences.", "https://www.imdb.com/title/tt0332280");
+var theProfessional = new Movie("The Professional", 1, 3, 4, 0, "img/the-professional.jpg", "Mathilda, a 12-year-old girl, is reluctantly taken in by Léon, a professional assassin, after her family is murdered. Léon and Mathilda form an unusual relationship, as she becomes his protégée and learns the assassin's trade.", "https://www.imdb.com/title/tt0110413");
+var hiddenFigures = new Movie("Hidden Figures", 3, 2, 4, 2, "img/hidden-figures.jpg", "The story of a team of female African-American mathematicians who served a vital role in NASA during the early years of the U.S. space program.", "https://www.imdb.com/title/tt4846340");
+var arrival = new Movie("Arrival", 2, 1, 3, 2, "img/arrival.jpg", "A linguist is recruited by the military to communicate with alien lifeforms after twelve mysterious spacecrafts land around the world.", "https://www.imdb.com/title/tt2543164");
+var profMarston = new Movie("Professor Marston and the Wonder Women", 3, 1, 3, 5, "img/prof-marston.jpg", "The story of psychologist William Moulton Marston, and his polyamorous relationship with his wife and their mistress who would inspire his creation of the superheroine, Wonder Woman.", "https://www.imdb.com/title/tt6133130");
 
 var masterMoviesLists = [schindlers, titanic, fault, coco, findingNemo, alice, gilmore, monsters, amelie, up, solo, kungFu, taken, fightClub, tarzan, perksWallflower, sleepless, fiveHundredDays, crazyStupid, blackPanther, eternalSunshine];
 
@@ -65,10 +70,8 @@ function sayIfHappy(happyInput, movieResults) {
   return newMovie;
 }
 
-
 function sayIfSad(sadInput, movieResults) {
   var newMovie = new Movie();
-
   masterMoviesLists.forEach(function(movie) {
     if (movie.sadCategory === 5 && sadInput === 5) {
       movieResults.push(movie);
@@ -133,11 +136,9 @@ function Movie(title, happyCategory, sadCategory, angstCategory, loveCategory, i
   this.link = link;
 }
 
-
 // user interface
 $(document).ready(function() {
 
-// buttons to show radios to rank emotions
   $("#happyButton").click(function(){
     $(".moodButtons").hide();
     $("#happySubmit").fadeIn();
@@ -163,7 +164,6 @@ $(document).ready(function() {
     $(".refresh").show();
 
     var movieResults = [];
-
     var happyInput = parseInt($("input[name='happyRange']").val());
     sayIfHappy(happyInput, movieResults);
 
@@ -173,8 +173,6 @@ $(document).ready(function() {
       var descId = "desc" + i;
       var linkId = "link" + i;
 
-      console.log(movieResults);
-
       $(".cardCode").append(
         "<div class='card showMovie' style='width: 18rem;' value='MOOD'>" +
         "<div class='card-body'>" +
@@ -199,8 +197,7 @@ $(document).ready(function() {
 
     };
   });
-  //
-  //
+
   $("#angstSubmit").submit(function(event) {
     event.preventDefault();
     $(".results").show();
@@ -208,7 +205,6 @@ $(document).ready(function() {
     $(".refresh").show();
 
     var movieResults = [];
-
     var angstInput = parseInt($("input[name='angstRange']").val());
     sayIfAngst(angstInput, movieResults);
 
@@ -218,8 +214,6 @@ $(document).ready(function() {
       var descId = "desc" + i;
       var linkId = "link" + i;
 
-      console.log(movieResults);
-
       $(".cardCode").append(
         "<div class='card showMovie' style='width: 18rem;' value='MOOD'>" +
         "<div class='card-body'>" +
@@ -234,18 +228,15 @@ $(document).ready(function() {
       var descIdInsert = '#' + descId;
       var linkIdInsert = '#' + linkId;
       var imgIdInsert = '#' + imgId;
-
       var currentMovie = movieResults[i];
 
         $(titleIdInsert).text(currentMovie.title);
         $(descIdInsert).text(currentMovie.description);
         $(linkIdInsert).html("<a href='" + (currentMovie.link) + "'>IMDb Profile</a>");
         $(imgIdInsert).html("<img class='card-img-top' src='" + (currentMovie.imgFile) + "'>");
-
     };
   });
-  //
-  //
+
   $("#loveSubmit").submit(function(event) {
     event.preventDefault();
     $(".results").show();
@@ -253,7 +244,6 @@ $(document).ready(function() {
     $(".refresh").show();
 
     var movieResults = [];
-
     var loveInput = parseInt($("input[name='loveRange']").val());
     sayIfLove(loveInput, movieResults);
 
@@ -263,8 +253,6 @@ $(document).ready(function() {
       var descId = "desc" + i;
       var linkId = "link" + i;
 
-      console.log(movieResults);
-
       $(".cardCode").append(
         "<div class='card showMovie' style='width: 18rem;' value='MOOD'>" +
         "<div class='card-body'>" +
@@ -279,7 +267,6 @@ $(document).ready(function() {
       var descIdInsert = '#' + descId;
       var linkIdInsert = '#' + linkId;
       var imgIdInsert = '#' + imgId;
-
       var currentMovie = movieResults[i];
 
         $(titleIdInsert).text(currentMovie.title);
@@ -290,8 +277,6 @@ $(document).ready(function() {
     };
   });
 
-
-
   $("#sadSubmit").submit(function(event) {
     event.preventDefault();
     $(".results").show();
@@ -299,7 +284,6 @@ $(document).ready(function() {
     $(".refresh").show();
 
     var movieResults = [];
-
     var sadInput = parseInt($("input[name='sadRange']").val());
     sayIfSad(sadInput, movieResults);
 
@@ -309,8 +293,6 @@ $(document).ready(function() {
       var descId = "desc" + i;
       var linkId = "link" + i;
 
-      console.log(movieResults);
-
       $(".cardCode").append(
         "<div class='card showMovie' style='width: 18rem;' value='MOOD'>" +
         "<div class='card-body'>" +
@@ -325,7 +307,6 @@ $(document).ready(function() {
       var descIdInsert = '#' + descId;
       var linkIdInsert = '#' + linkId;
       var imgIdInsert = '#' + imgId;
-
       var currentMovie = movieResults[i];
 
         $(titleIdInsert).text(currentMovie.title);
@@ -335,36 +316,4 @@ $(document).ready(function() {
 
     };
   });
-
-
-  });
-
-  var meetJoeBlack = new Movie("Meet Joe Black", 1, 3, 3, 3, "img/meet-joe-black.jpg", "Death, who takes the form of a young man, asks a media mogul to act as a guide to teach him about life on Earth, and in the process, he falls in love with his guide's daughter.", "http://www.imdb.com/title/tt0119643");
-
-var departed = new Movie("The Departed", 0, 2, 5, 0, "img/departed.jpg", "An undercover cop and a mole in the police attempt to identify each other while infiltrating an Irish gang in South Boston.", "https://www.imdb.com/title/tt0407887");
-
-var hook = new Movie("Hook", 4, 1, 2, 2, "img/hook.jpg", "When Captain Hook kidnaps his children, an adult Peter Pan must return to Neverland and reclaim his youthful spirit in order to challenge his old enemy.", "https://www.imdb.com/title/tt0102057");
-
-var leagueofTheirOwn = new Movie("A League of Their Own", 3, 1, 2, 2, "img/league-of-their-own.jpg", "Two sisters join the first female professional baseball league and struggle to help it succeed amidst their own growing rivalry.", "https://www.imdb.com/title/tt0104694");
-
-var iamLegend = new Movie("I Am Legend", 0, 5, 4, 0, "img/i-am-legend.jpg", "Years after a plague kills most of humanity and transforms the rest into monsters, the sole survivor in New York City struggles valiantly to find a cure.", "https://www.imdb.com/title/tt0480249");
-
-var getOut = new Movie("Get Out", 0, 2, 5, 1, "img/get-out.jpg", "A young African-American visits his white girlfriend's parents for the weekend, where his simmering uneasiness about their reception of him eventually reaches a boiling point.", "https://www.imdb.com/title/tt5052448");
-
-var aQuietPlace = new Movie("A Quiet Place", 0, 5, 5, 1, "img/quiet-place.jpg", "In a post-apocalyptic world, a family is forced to live in silence while hiding from monsters with ultra-sensitive hearing.", "https://www.imdb.com/title/tt6644200");
-
-====
-
-var rbg = new Movie("RBG", 3, 1, 2, 3, "img/rbg.jpg", "The exceptional life and career of U.S. Supreme Court Justice Ruth Bader Ginsburg, who has developed a breathtaking legal legacy while becoming an unexpected pop culture icon.", "https://www.imdb.com/title/tt7689964");
-
-var wonderWoman = new Movie("Wonder Woman", 2, 2, 3, 3, "img/wonder-woman.jpg", "When a pilot crashes and tells of conflict in the outside world, Diana, an Amazonian warrior in training, leaves home to fight a war, discovering her full powers and true destiny.", "https://www.imdb.com/title/tt0451279");
-
-var notebook = new Movie("The Notebook", 3, 4, 2, 5, "img/notebook.jpg", "A poor yet passionate young man falls in love with a rich young woman, giving her a sense of freedom, but they are soon separated because of their social differences.", "https://www.imdb.com/title/tt0332280");
-
-var theProfessional = new Movie("The Professional", 1, 3, 4, 0, "img/the-professional.jpg", "Mathilda, a 12-year-old girl, is reluctantly taken in by Léon, a professional assassin, after her family is murdered. Léon and Mathilda form an unusual relationship, as she becomes his protégée and learns the assassin's trade.", "https://www.imdb.com/title/tt0110413");
-
-var hiddenFigures = new Movie("Hidden Figures", 3, 2, 4, 2, "img/hidden-figures.jpg", "The story of a team of female African-American mathematicians who served a vital role in NASA during the early years of the U.S. space program.", "https://www.imdb.com/title/tt4846340");
-
-var arrival = new Movie("Arrival", 2, 1, 3, 2, "img/arrival.jpg", "A linguist is recruited by the military to communicate with alien lifeforms after twelve mysterious spacecrafts land around the world.", "https://www.imdb.com/title/tt2543164");
-
-var profMarston = new Movie("Professor Marston and the Wonder Women", 3, 1, 3, 5, "img/prof-marston.jpg", "The story of psychologist William Moulton Marston, and his polyamorous relationship with his wife and their mistress who would inspire his creation of the superheroine, Wonder Woman.", "https://www.imdb.com/title/tt6133130");
+});
